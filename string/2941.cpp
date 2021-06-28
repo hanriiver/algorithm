f037f7424b6014ee55609a7af2da4c32a6758867
kitten-master <dgdev@naver.com>
2021-06-28 20:59:56 +0900

    크로아티아 알파벳 fixed

--- string/2941.cpp
+++ string/2941.cpp
@@ -51,3 +51,21 @@ int main() {
         cout << n + ans;
     }
 }
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    vector<string> v = { "c=","c-","dz=","d-","lj","nj","s=","z=" };
+    string s;
+    cin >> s;
+    for (int i = 0; i < v.size(); i++) {
+        while (1) {
+            int idx = s.find(v[i]);
+            if (idx == string::npos)
+                break;
+            s.replace(idx, v[i].size(), "#");
+        }
+    }
+    cout << s.size();
+}
