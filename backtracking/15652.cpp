545aa9f600b74ad4dd82bdff6489d0092db57ea4
kitten <doraemon216@nate.com>
2021-07-26 21:14:40 +0900

    N과 M(4)

---
+++ backtracking/15652.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int n, m;
+int arr[9];
+vector<int> ans;
+
+void go(int start, int index) {
+	if (index == m) {
+		for (int i = 0; i < m; i++) {
+			cout << ans[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = start; i < n; i++) {
+		ans.push_back(arr[i]);
+		go(i, index + 1);
+		ans.pop_back();
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) arr[i] = i + 1;
+	go(0, 0);
+}
