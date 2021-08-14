908cb84c17ec81f80ef3c12d00c58b3c79136d18
kitten <doraemon216@nate.com>
2021-08-14 13:04:46 +0900

    정수 삼각형

---
+++ dynamic programming/1932.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int arr[501][501];
+int dp[501][501];
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    dp[1][1] = arr[1][1];
+    for (int i = 2; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + arr[i][j];
+        }
+    }
+    int ans = 0;
+    for (int i = 1; i <= n; i++) ans = max(ans, dp[n][i]);
+    cout << ans;
+}
