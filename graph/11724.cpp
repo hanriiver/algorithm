3141818ac51b823a1a0e6bb21ca531e7efd98879
kitten <doraemon216@nate.com>
2021-08-17 20:17:00 +0900

    연결 요소(dfs)

--- graph/11724.cpp
+++ graph/11724.cpp
@@ -40,3 +40,40 @@ int main() {
 	}
 	cout << ans;
 }
+/////////////////////////////////////////////////////////////////
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include<memory.h>
+using namespace std;
+int n, m;
+vector<int> g[1001];
+bool check[1001];
+void dfs(int index) {
+	check[index] = true;
+	for (int i = 0; i < g[index].size(); i++) {
+		int next = g[index][i];
+		if (check[next]) continue;
+		check[next] = true;
+		dfs(next);
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(false), cin.tie(NULL);
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		int u, v;
+		cin >> u >> v;
+		g[u].push_back(v);
+		g[v].push_back(u);
+	}
+	int ans = 0;
+	for (int i = 1; i <= n; i++) {
+		if (!check[i]) {
+			dfs(i);
+			ans++;
+		}
+	}
+	cout << ans;
+}
