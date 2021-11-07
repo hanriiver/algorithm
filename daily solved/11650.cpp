f296cf764858e182650a9881ec95f8bbc16ef377
chefkitten <dgdev@naver.com>
2021-11-07 13:04:36 +0900

    좌표 정렬하기

---
+++ daily solved/11650.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+vector<pair<int, int>> v;
+bool cmp(pair<int, int> v1, pair<int, int> v2) {
+	if (v1.first == v2.first) {
+		return v1.second < v2.second;
+	}
+
+	return v1.first < v2.first;
+
+}
+int main() {
+	ios_base::sync_with_stdio(false), cin.tie(NULL);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int x, y;
+		cin >> x >> y;
+		v.push_back({ x,y });
+	}
+	sort(v.begin(), v.end(), cmp);
+	for (auto k : v) {
+		cout << k.first << " " << k.second << "\n";
+	}
+}
