6b869ff95783a5498b6c16185529544d0c34374b
riverrevir <81510864+riverrevir@users.noreply.github.com>
2022-02-28 18:55:39 +0900

    3주차 스터디

---
+++ study/3주차/12851.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, k;
+int check[200001];
+int ans[200001];
+queue<int> qu;
+int cal(int start, int oper) {
+    if (oper == 0) {
+        return start + 1;
+    }
+    else if (oper == 1) {
+        return start - 1;
+    }
+    else return start * 2;
+}
+void go(int start) {
+    qu.push(start);
+    check[start] = 0;
+    ans[start] = 1;
+    while (!qu.empty()) {
+        int os = qu.front();
+        qu.pop();
+        if (os == k) {
+            return;
+        }
+        for (int i = 0; i < 3; i++) {
+            int ns = cal(os, i);
+            if (ns < 0 || ns>200000) continue;
+            if (check[ns] == check[os] + 1) ans[ns] += ans[os];
+            if (check[ns] != -1) continue;
+            check[ns] = check[os] + 1;
+            ans[ns] += ans[os];
+            qu.push(ns);
+        }
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(NULL);
+    cin >> n >> k;
+    memset(check, -1, sizeof(check));
+    go(n);
+    cout << check[k] << "\n" << ans[k];
+}
+
