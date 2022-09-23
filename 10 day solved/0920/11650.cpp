a85f9c6f44310bc1b90570a0558af153c6a19d66
riverrevir <81510864+riverrevir@users.noreply.github.com>
2022-09-23 21:58:30 +0900

    좌표 정렬하기

---
+++ 10 day solved/0920/11650.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<pair<int, int>> v;
+bool cmp(pair<int, int> a, pair<int, int> b) {
+	if (a.first < b.first) {
+		return true;
+	}
+	else if (a.first == b.first) {
+		if (a.second < b.second) {
+			return true;
+		}
+		else return false;
+	}
+	return false;
+}
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int x, y;
+		cin >> x >> y;
+		v.push_back({ x,y });
+	}
+	sort(v.begin(), v.end(), cmp);
+	for (int i = 0; i < n; i++) {
+		cout << v[i].first << " " << v[i].second << "\n";
+	}
+}
