f1928883bef3ee085a5b7337023fdee99749ce7d
hanriver <81510864+hanriiver@users.noreply.github.com>
2025-05-03 20:44:34 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false), cin.tie(NULL);
+    int a,b;
+    cin>>a>>b;
+    if(a>b) cout<<">";
+    else if(a==b) cout<<"==";
+    else cout<<"<";
+}
