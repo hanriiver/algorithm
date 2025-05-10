ab438de881b1381d20144ca023c8057bab462e15
hanriver <81510864+hanriiver@users.noreply.github.com>
2025-05-10 16:52:20 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 196 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL);
+    int T;
+    cin>>T;
+    while(T--){
+        int a,b;
+        cin>>a>>b;
+        cout<<a+b<<"\n";
+    }
+}
