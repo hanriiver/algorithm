c3b682b533cfd0dcaed89a7bd9d8149169afc470
hanriver <81510864+hanriiver@users.noreply.github.com>
2025-05-23 17:06:00 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int a, b, c, d, e, f;
+	cin >> a >> b >> c >> d >> e >> f;
+	cout << 1 - a << " " << 1 - b << " " << 2 - c << " " << 2 - d << " " << 2 - e << " " << 8 - f;
+}
