e122a96133b100c10e1d84f9ca8f57b189bdc64d
dg <81510864+hanriiver@users.noreply.github.com>
2025-07-06 03:25:58 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    vector<string> v = { "c=","c-","dz=","d-","lj","nj","s=","z=" };
+    string s;
+    cin >> s;
+    for (int i = 0; i < v.size(); i++) {
+        while (1) {
+            int idx = s.find(v[i]);
+            if (idx == string::npos)
+                break;
+            s.replace(idx,v[i].size(),"#");
+        }
+    }
+    cout << s.size();
+}
