0f7cce3700172672437753f187b137c308cf6672
dg <81510864+hanriiver@users.noreply.github.com>
2025-07-07 23:18:47 +0900

    [Bronze II] Title: 분해합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <map>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false), cin.tie(NULL);
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		int temp = i;
+		int sum = i;
+		while (temp>0) {
+			sum += (temp % 10);
+			temp /= 10;
+		}
+		if (sum == n) {
+			cout << i;
+			return 0;
+		}
+	}
+	cout << 0;
+}
+
