5d2244ed0ae28f6b88cf78aad0a0b11c8e033702
dg <81510864+hanriiver@users.noreply.github.com>
2025-07-11 02:05:19 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <map>
+#include <iomanip>
+#include <set>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL);
+    int n,k;
+    cin>>n>>k;
+    vector<int> v;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+    sort(v.begin(),v.end());
+    cout<<v[v.size()-k];
+}
