3e802b72d5a9d9c0a45cba7c46f76d6f4cbaaae3
dg <81510864+hanriiver@users.noreply.github.com>
2025-07-18 23:28:01 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <map>
+#include <iomanip>
+#include <set>
+#include <queue>
+#include <deque>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL);
+    int n;
+    cin>>n;
+    cout<<(1<<n);
+}
