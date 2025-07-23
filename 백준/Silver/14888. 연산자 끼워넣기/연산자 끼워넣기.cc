98b838f4514615d3933bbb5907dd47b0712be2d3
dg <81510864+hanriiver@users.noreply.github.com>
2025-07-23 20:25:22 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <limits.h>
+using namespace std;
+int n,max_=INT_MIN,min_=INT_MAX;
+vector<int> arr;
+
+int oper[4];
+void go(int sum, int index) {
+	if (index == n) {
+		if (sum > max_) max_ = sum;
+		if (sum < min_) min_ = sum;
+		return;
+	}
+	for (int i = 0; i < 4; i++) {
+		if (oper[i]) {
+			if (i == 0) {
+				oper[i]--;
+				go(sum+arr[index], index + 1);
+				oper[i]++;
+			}
+			else if (i == 1) {
+				oper[i]--;
+				go(sum - arr[index], index + 1);
+				oper[i]++;
+			}
+			else if (i == 2) {
+				oper[i]--;
+				go(sum *arr[index], index + 1);
+				oper[i]++;
+			}
+			else if (i == 3) {
+				oper[i]--;
+				go(sum / arr[index], index + 1);
+				oper[i]++;
+			}
+		}
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+	 
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		arr.push_back(x);
+	}
+	for (int i = 0; i < 4; i++) cin >> oper[i];
+	go(arr[0],1);
+	cout << max_ << "\n" << min_;
+}
